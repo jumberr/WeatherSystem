007c8b175836366b80332a25e65371d68c9cb00b
jumberr <tzzader@gmail.com>
2021-11-06 12:06:15 +0200

    Lab 7.

---
+++ DAL.Tests/BaseRepositoryUnitTests.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DAL.Entities;
+using DAL.Repository.Impl;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace DAL.Tests
+{
+    public class BaseRepositoryUnitTests
+    {
+        [Fact]
+        public void Create_InputCalculatedStatsInstance_CalledAddMethodOfDBSetWithCalculatedStatsInstance()
+        {
+            // Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<StatsContext>()
+                .Options;
+            var mockContext = new Mock<StatsContext>(opt);
+            var mockDbSet = new Mock<DbSet<CalculatedStats>>();
+            mockContext
+                .Setup(context => 
+                    context.Set<CalculatedStats>(
+                    ))
+                .Returns(mockDbSet.Object);
+            var repository = new TestCalcStatsRepository(mockContext.Object);
+            var expectedStats = new Mock<CalculatedStats>().Object;
+
+            //Act
+            repository.Create(expectedStats);
+
+            // Assert
+            mockDbSet.Verify(
+                dbSet => dbSet.Add(
+                    expectedStats
+                ), Times.Once());
+        }
+        
+        [Fact]
+        public void Delete_InputId_CalledFindAndRemoveMethodsOfDBSetWithCorrectArg()
+        {
+            // Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<StatsContext>()
+                .Options;
+            var mockContext = new Mock<StatsContext>(opt);
+            var mockDbSet = new Mock<DbSet<CalculatedStats>>();
+            mockContext
+                .Setup(context =>
+                    context.Set<CalculatedStats>(
+                        ))
+                .Returns(mockDbSet.Object);
+            //EFUnitOfWork uow = new EFUnitOfWork(mockContext.Object);
+            //IStreetRepository repository = uow.Streets;
+            var repository = new TestCalcStatsRepository(mockContext.Object);
+
+            var expectedStats = new CalculatedStats { ID = 1};
+            mockDbSet.Setup(mock => mock.Find(expectedStats.ID)).Returns(expectedStats);
+
+            //Act
+            repository.Delete(expectedStats.ID);
+
+            // Assert
+            mockDbSet.Verify(
+                dbSet => dbSet.Find(
+                    expectedStats.ID
+                    ), Times.Once());
+            mockDbSet.Verify(
+                dbSet => dbSet.Remove(
+                    expectedStats
+                    ), Times.Once());
+        }
+
+        [Fact]
+        public void Get_InputId_CalledFindMethodOfDBSetWithCorrectId()
+        {
+            // Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<StatsContext>()
+                .Options;
+            var mockContext = new Mock<StatsContext>(opt);
+            var mockDbSet = new Mock<DbSet<CalculatedStats>>();
+            mockContext
+                .Setup(context =>
+                    context.Set<CalculatedStats>(
+                        ))
+                .Returns(mockDbSet.Object);
+
+            var expectedStats = new CalculatedStats { ID = 1 };
+            mockDbSet.Setup(mock => mock.Find(expectedStats.ID))
+                    .Returns(expectedStats);
+            var repository = new TestCalcStatsRepository(mockContext.Object);
+
+            //Act
+            var actualStreet = repository.Get(expectedStats.ID);
+
+            // Assert
+            mockDbSet.Verify(
+                dbSet => dbSet.Find(
+                    expectedStats.ID
+                    ), Times.Once());
+            Assert.Equal(expectedStats, actualStreet);
+        }
+
+        [Fact]
+        public void Get_InputId_CalledFindMethodOfDbSetWithCorrect()
+        {
+            // Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<StatsContext>()
+                .Options;
+            var mockContext = new Mock<StatsContext>(opt);
+            var mockDbSet = new Mock<DbSet<CalculatedStats>>();
+            mockContext
+                .Setup(context =>
+                    context.Set<CalculatedStats>(
+                    ))
+                .Returns(mockDbSet.Object);
+
+            
+            var expected = new List<CalculatedStats> {new(){ID = 1}};
+            mockDbSet.Setup(mock => mock.ToList()).Returns(expected);
+
+            //Act
+            var repository = new TestCalcStatsRepository(mockContext.Object);
+            var actual = repository.GetAll();
+
+            // Assert
+            mockDbSet.Verify(
+                dbSet => dbSet.ToList(), Times.Once());
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DAL.Repository.Impl;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        RawStatsRepository RawStats { get; }
+        CalculatedStatsRepository CalculatedStats { get; }
+        void Save();
+    }
+}
